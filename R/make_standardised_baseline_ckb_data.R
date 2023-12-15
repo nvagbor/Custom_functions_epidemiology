5cf60c5efea398d1132374c3608d232d6628bde7
Valirie N. Agbor <56177423+nvagbor@users.noreply.github.com>
2023-12-15 11:50:04 +0000

    Update make_standardised_baseline_ckb_data.R
    Format f(x) was used to edit decimals instead of round.

--- R/make_standardised_baseline_ckb_data.R
+++ R/make_standardised_baseline_ckb_data.R
@@ -1,32 +1,30 @@
 
 # Function to create data for standardised means and proportions for CKB data
 # Data can be edited with other packages like Flextable to make publication-ready tables 
-# Package dependencies: Tidyverse, directStandardisation package
+# Package dependencies: tidyr, dplyr, rlang, directStandardisation package
+# The function returns a tibble
 
 # The main exposure must be a categorical variable
 
-make_standardised_ckb_data <- function(dataset, 
-                                       main_exposure,
-                                       numeric_vars = NULL, 
-                                       numeric_vars_labels = NULL, 
-                                       categorical_vars = NULL, 
-                                       categorical_vars_labels = NULL, ## Preferable to not have this in
-                                       adjustment_variables, 
-                                       sex_specific_analysis = FALSE,
-                                       sex_var = NULL,
-                                       n_decimals = 1
-                                       ) {
+make_standardised_ckb_data <- function(
+   dataset, 
+   main_exposure,
+   numeric_vars = NULL, 
+   numeric_vars_labels = NULL, 
+   categorical_vars = NULL, 
+   categorical_vars_labels = NULL, ## Preferable to not have this in
+   adjustment_variables, 
+   sex_specific_analysis = FALSE, ## To perform analyses for men and women (could be any other categorical variable)
+   sex_var = NULL,
+   n_decimals = 1
+   ) {
 
 
   # Checks ---
   
     if(missing(main_exposure)) stop("please enter `main exposure`")
-      
-    if(!is.factor(dataset[[main_exposure]])) {
-      stop("The `main exposure` must be a factor")  
-      }
-  
-  
+    if(!is.factor(dataset[[main_exposure]])) stop("The `main exposure` must be a factor")  
+
     # Verify length if variable names and labels are provided 
     if(!is.null(numeric_vars) & !is.null(numeric_vars_labels)){
       
@@ -42,7 +40,7 @@ make_standardised_ckb_data <- function(dataset,
     
   # Force dataset into a dataframe 
     
-  dataset <- dataset %>% as.data.frame(.)
+  dataset <- as.data.frame(dataset)
     
     
   # 1. For NUMERIC variables ----------------------------------------------------------------
@@ -57,26 +55,21 @@ if(!is.null(numeric_vars)){
     
     for (nv in seq_along(numeric_vars)) {
       
-      num_var_temp <- numeric_vars[[nv]]
+      num_var_temp <- numeric_vars[[nv]]   
       
-      
-      numeric_vars_df[[nv]] <- 
-        
-        DirectStandardisation::adjmeans(
+      numeric_vars_df[[nv]] <- DirectStandardisation::adjmeans(
           dataset           = dataset_temp, 
           outcome_vars      = num_var_temp,
           categorical_vars  = main_exposure, 
           adjustment_vars   = adjustment_variables, 
-          ndigits           = 4
-          
-        ) %>% 
+          ndigits           = 4) %>% 
         
         # Wrangling 
         transmute(
           cat_var_lab = !!rlang::sym(paste0(num_var_temp, ".levels")),
           stdev       = !!sym(paste0(num_var_temp, ".se")) * sqrt( !!sym(paste0(num_var_temp, ".N.Freq")) ), 
-          mean_sd     = paste0(round( !!sym(paste0(num_var_temp, ".mean")), n_decimals), 
-                               " (", round(stdev, n_decimals), ")")
+          mean_sd     = paste0(format( !!sym(paste0(num_var_temp, ".mean")), nsmall = n_decimals), 
+                               " (", format(stdev, nsmall = n_decimals), ")")
           )  %>% 
         select(-stdev) %>% 
       
@@ -84,14 +77,13 @@ if(!is.null(numeric_vars)){
       
         mutate(
           exposure = num_var_temp, 
-          all = paste0(round(mean(dataset_temp [ , num_var_temp], na.rm = TRUE), n_decimals), 
-                       " (", round(sd  (dataset_temp [ , num_var_temp], na.rm = TRUE), n_decimals), ")")
+          all = paste0(format(mean(dataset_temp [ , num_var_temp], na.rm = TRUE), nsmall = n_decimals), 
+                       " (", format(sd  (dataset_temp [ , num_var_temp], na.rm = TRUE), nsmall = n_decimals), ")")
           ) %>% 
         relocate(.before = 1, exposure)
       
       # Assign labels 
       if(!is.null(numeric_vars_labels)) {numeric_vars_df[[nv]]$exposure <- numeric_vars_labels[[nv]]}
-      
       
       # End of For Loop
     }
@@ -104,7 +96,7 @@ if(!is.null(numeric_vars)){
   
   ## 2. For CATEGORICAL VARIABLES ---
 
-  if(sex_specific_analysis == TRUE){
+  if(!sex_specific_analysis){
     
     dataset_temp <- 
       dataset %>% 
@@ -121,7 +113,6 @@ if(!is.null(numeric_vars)){
   
 if(!is.null(categorical_vars)){
   
-  
   cat_var_list <- list()
   
   for (c in seq_along(categorical_vars)) {
@@ -130,41 +121,29 @@ if(!is.null(categorical_vars)){
     
     # Get adjusted proportions ---
     
-    cat_var_list[[c]] <- 
+    cat_var_list[[c]] <- DirectStandardisation::adjprop(
+        dataset          = dataset_temp,
+        outcome_vars     = cat_var_temp,
+        categorical_vars = main_exposure,
+        adjustment_vars  = adjustment_variables) %>% 
       
-      DirectStandardisation::adjprop(
-        
-        dataset                = dataset_temp,
-        outcome_vars           = cat_var_temp,
-        categorical_vars       = main_exposure,
-        adjustment_vars        = adjustment_variables
-        
-      ) %>% 
-      
-      dplyr::mutate(across(contains("proportion"), ~round(.*100, n_decimals)) %>% 
-      
+      dplyr::mutate(across(contains("proportion"), ~format(.*100, nsmall = n_decimals)) %>% 
       select(contains(c("levels", "Freq", "proportion"))) %>% 
       `row.names<-` (levels (dataset_temp[[main_exposure]]) %>%  
       t(.) %>% 
-      
       `row.names<-` (
-        
         str_replace_all(string      = row.names(.), 
                         pattern     = paste0(cat_var_temp, "."), 
                         replacement = ""
-                       ) %>% 
-                      
+                       ) %>%     
         str_replace_all(pattern = "proportion.", replacement = "")
         
-        ) %>% 
-      
-      as.data.frame(.) 
+        ) %>%  as.data.frame(.) 
     
-          # If sex-specific analysis, define exposure differently
+      # If sex-specific analysis, define exposure differently
       if(sex_specific_analysis) {
         
         cat_var_list[[c]] <- 
-          
           cat_var_list[[c]] %>% 
           dplyr::mutate(exposure = paste0(unique(dataset_temp[[sex_var]]), "_", cat_var_temp, "_", row.names(.)))%>% 
           dplyr::relocate("exposure", .before = levels (dataset_temp[[main_exposure]])[1]) %>% 
@@ -184,20 +163,17 @@ if(!is.null(categorical_vars)){
     # Get overall proportions 
     
     prop_overall <- 
-      
       dataset_temp %>% 
           dplyr::group_by( .data[[cat_var_temp]] ) %>% 
           dplyr::summarise(
             n = n(), 
             N = length( dataset_temp[[cat_var_temp]] ), 
-            prop100 = round((n/N)*100, n_decimals)
+            prop100 = format((n/N)*100, nsmall = n_decimals)
             ) %>% 
           dplyr::select(all_of(cat_var_temp), prop100)
-     
     
     # Bind overall proportion df to main dataframe 
-    cat_var_list[[c]]$all <-  prop_overall$prop100
-    
+    cat_var_list[[c]]$all <-  prop_overall$prop100  
     
     # End of For Loop
   }
@@ -226,8 +202,7 @@ if(!is.null(categorical_vars)){
     if(is.null(numeric_vars) & !is.null(categorical_vars)){
       df_stdz <- df_cat_var
       return(df_stdz)
-    }
-  
+    } 
   
 # End of Function
 }
